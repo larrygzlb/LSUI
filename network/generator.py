f1d6c4686c4194fe521f8de23a43e7771e0a94b3
unknown <larrygzlb@gmail.com>
2024-03-11 00:32:56 +0100

    draft

---
+++ network/generator.py
@@ -0,0 +1,3 @@
+# A DCNN
+
+MyGenerator()
--- utils/mydataset.py
+++ utils/mydataset.py
@@ -7,26 +7,23 @@ from PIL import Image
 import os
 from torchvision.io import read_image
 
+
 class MyDataset(Dataset):
     def __init__(self, input_folder, truth_folder, transform=None):
         self.input_folder = input_folder
         self.truth_folder = truth_folder
         self.transform = transform
-        
-        self.image_filenames = [f for f in os.listdir(input_folder) if os.path.isfile(os.path.join(truth_folder, f))]
+        self.image_filenames = os.listdir(input_folder)
 
     def __len__(self):
         return len(self.image_filenames)
 
     def __getitem__(self, idx):
-        input_path = os.path.join(self.input_folder, self.image_filenames[idx])
-        truth_path = os.path.join(self.truth_folder, self.image_filenames[idx])
-        
-        input_image = read_image(input_path) / 255.0
-        truth_image = read_image(truth_path) / 255.0
+        input_image = (read_image(os.path.join(self.input_folder, self.image_filenames[idx])))/255.0
+        truth_image = (read_image(os.path.join(self.truth_folder, self.image_filenames[idx])))/255.0
 
         if self.transform:
             input_image = self.transform(input_image)
             truth_image = self.transform(truth_image)
 
-        return input_image, truth_image, self.image_filenames[idx]
+        return input_image, truth_image
